0c62ea9b59c74d306a6bcf1addd958775e8cd260
choldener <choldener@users.noreply.github.com>
2020-09-06 03:15:31 -0500

    Update tweets_to_images.py
    Added computer vision date location dection

--- tweets_to_images.py
+++ tweets_to_images.py
@@ -8,13 +8,19 @@ import os
 import fire
 import errno
 import random
+import cv2 
 
 def tweets_to_images(
 file, 
 tweet_image,
 bar = False,
-date = False
+date = False #adding date dramatically decreases performance due to addition of computer vision
 ):
+    image = Image.open(tweet_image)
+    base_width, base_height = image.size
+    if base_width != 1500 & base_height != 300: raise #base_width / base_height != 5: raise
+    else: pass
+
     try:
         os.makedirs('output')
     except OSError as e:
@@ -39,40 +45,45 @@ date = False
             bar_info(image, draw, x_text, y_text)
         image.save('output/output_' + str(idx+1) + '.png')
 
-def testing_function (file, tweet_image, info = True, date = True, bar = True):
-    image = Image.open(tweet_image)
-    draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype(r'C:\Windows\Fonts\Segoeuisl.ttf', size=37)
-    tweet = ""
-    tweet = "\n".join(textwrap.wrap(tweet, width=75))
-    draw.text ((185,80), tweet, font = font, fill = (0,0,0))
-    x_text,y_text = draw.textsize(tweet, font=font)
-    x_text,y_text = x_text + 85,y_text + 85
-    # if date == True:
-    #     date_info()
-    if bar == True:
-        bar_info(image, draw, x_text, y_text)
-    image.save(r'D:\Github\Projects\Tweet-to-image\output4.png')
+# def testing_function (tweet_image, date = True, bar = True):
+#     image = Image.open(tweet_image)
+#     draw = ImageDraw.Draw(image)
+#     font = ImageFont.truetype(r'C:\Windows\Fonts\Segoeuisl.ttf', size=37)
+#     tweet = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+#     tweet = "\n".join(textwrap.wrap(tweet, width=75))
+#     draw.text ((185,80), tweet, font = font, fill = (0,0,0))
+#     x_text,y_text = draw.textsize(tweet, font=font)
+#     x_text,y_text = x_text + 85,y_text + 85
+#     if date == True:
+#         date_info(image, draw, tweet_image)
+#     if bar == True:
+#         bar_info(image, draw, x_text, y_text)
+#     image.save(r'D:\Github\Projects\Tweet-to-image\output4.png')
 
 def bar_info(image, draw, x_text, y_text):
     box = Image.open(r'resources\bar_no_data.png')
-    font = ImageFont.truetype(r'C:\Windows\Fonts\Segoeuisl.ttf', size=35)
+    font = ImageFont.truetype(r'C:\Windows\Fonts\Segoeui.ttf', size=35)
     image.paste(box, box=(185, y_text))
-    date = random.randrange(0,1)
-    print('parmas set')
-    print(draw) 
-    ######## Still need to figure out coords on image
     draw.text((250, y_text+19), str(random.randrange(0,100)), font=font, fill = (101,119,134))
     draw.text((565, y_text+19), str(random.randrange(0,1000)), font=font, fill = (101,119,134))
     draw.text((865, y_text+19), str(random.randrange(0,1000)), font=font, fill = (101,119,134))
-    draw.text((950, 25), str(date), font=font, fill = (101,119,134))
-    ########
 
-def date_info(image, draw):
-    box = Image.open(r'resources\date_dot.png')
+def date_info(image, draw, tweet_image): #have to utilize cv2 due to variable name length
+    img = cv2.imread(tweet_image)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+    ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15,15)) 
+    dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1) 
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+    cnt = contours[1]
+    x,y,w,h = cv2.boundingRect(cnt)
+    # cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+    # cv2.imshow("Wall Detection", img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    
     font = ImageFont.truetype(r'C:\Windows\Fonts\Segoeuisl.ttf', size=35)
-    image.paste(box, box=(950, 25))
-    draw.text((950,25), ' · '+str(random.randrange(1,23)) + 'h', font = font, fill = (101,119,134))
+    draw.text((x+w-4,y+1), ' · '+str(random.randrange(1,23)) + 'h', font = font, fill = (101,119,134))
     
 if __name__ == "__main__":
     fire.Fire(tweets_to_images)
